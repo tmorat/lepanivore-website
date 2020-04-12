516ec6e4327f7b81987b73d1a1f8610fe2fb0297
Damien Beaufils <damien.beaufils@gmail.com>
2020-04-12 16:00:47 -0400

    💚 use latest clever tools version

--- tools/deploy-to-clever-cloud.sh
+++ tools/deploy-to-clever-cloud.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-curl https://clever-tools.cellar.services.clever-cloud.com/releases/latest/clever-tools-latest_linux.tar.gz > clever-tools-latest_linux.tar.gz
-tar -xvf clever-tools-latest_linux.tar.gz --strip-components=1
-
-./clever login --token $CLEVER_TOKEN --secret $CLEVER_SECRET
+clever login --token $CLEVER_TOKEN --secret $CLEVER_SECRET
 rm -f .clever.json
 
 echo "Deploying backend"
-./clever link -a backend_app $BACKEND_APP_ID
-./clever deploy -a backend_app -f
+clever link -a backend_app $BACKEND_APP_ID
+clever deploy -a backend_app -f
 
 echo "Deploying frontend"
-./clever link -a frontend_app $FRONTEND_APP_ID
-./clever deploy -a frontend_app -f
+clever link -a frontend_app $FRONTEND_APP_ID
+clever deploy -a frontend_app -f
