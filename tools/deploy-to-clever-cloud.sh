b982bc58adc10101f1c50eef279f991ba6a59e92
Damien Beaufils <damien.beaufils@gmail.com>
2020-05-05 16:53:59 -0400

    🚀 add timeout to clever deploy commands

--- tools/deploy-to-clever-cloud.sh
+++ tools/deploy-to-clever-cloud.sh
@@ -5,8 +5,8 @@ rm -f .clever.json
 
 echo "Deploying backend"
 clever link -a backend_app $BACKEND_APP_ID
-clever deploy -a backend_app -f
+timeout --signal=SIGINT 360 clever deploy -a backend_app -f || (timeout --signal=SIGINT 120 clever restart -a backend_app || true)
 
 echo "Deploying frontend"
 clever link -a frontend_app $FRONTEND_APP_ID
-clever deploy -a frontend_app -f
+timeout --signal=SIGINT 360 clever deploy -a frontend_app -f || (timeout --signal=SIGINT 120 clever restart -a frontend_app || true)
