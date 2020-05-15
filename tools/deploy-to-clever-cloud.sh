509586ee6c2d35d8d410d225156ccd4946ba2d32
Damien Beaufils <damien.beaufils@gmail.com>
2020-05-15 11:56:29 -0400

    🚀 prepare for production deployment

--- tools/deploy-to-clever-cloud.sh
+++ tools/deploy-to-clever-cloud.sh
@@ -3,10 +3,29 @@
 clever login --token $CLEVER_TOKEN --secret $CLEVER_SECRET
 rm -f .clever.json
 
-echo "Deploying backend"
-clever link -a backend_app $BACKEND_APP_ID
-timeout --signal=SIGINT 360 clever deploy -a backend_app -f || (timeout --signal=SIGINT 120 clever restart -a backend_app || true)
+echo "Deploying backend DEV"
+clever link -a backend_dev_app $BACKEND_DEV_APP_ID
+timeout --signal=SIGINT 360 clever deploy -a backend_dev_app -f || (timeout --signal=SIGINT 120 clever restart -a backend_dev_app || true)
 
-echo "Deploying frontend"
-clever link -a frontend_app $FRONTEND_APP_ID
-timeout --signal=SIGINT 360 clever deploy -a frontend_app -f || (timeout --signal=SIGINT 120 clever restart -a frontend_app || true)
+echo "Deploying frontend DEV"
+clever link -a frontend_dev_app $FRONTEND_DEV_APP_ID
+timeout --signal=SIGINT 360 clever deploy -a frontend_dev_app -f || (timeout --signal=SIGINT 120 clever restart -a frontend_dev_app || true)
+
+if [ -z "$DEPLOY_TO_PROD" ]
+then
+  echo "\$DEPLOY_TO_PROD is empty, will NOT deploy to prod"
+else
+  echo "\$DEPLOY_TO_PROD is NOT empty, will now deploy to prod!"
+
+  echo "Deploying backend PROD"
+  clever link -a backend_prod_app $BACKEND_PROD_APP_ID
+  timeout --signal=SIGINT 360 clever deploy -a backend_prod_app -f || (timeout --signal=SIGINT 120 clever restart -a backend_prod_app || true)
+
+  echo "Deploying frontend PROD"
+  clever link -a frontend_prod_app $FRONTEND_PROD_APP_ID
+  timeout --signal=SIGINT 360 clever deploy -a frontend_prod_app -f || (timeout --signal=SIGINT 120 clever restart -a frontend_prod_app || true)
+fi
+
+# To deploy to prod, trigger a custom build with this YAML custom config:
+# env:
+#   - DEPLOY_TO_PROD=true
